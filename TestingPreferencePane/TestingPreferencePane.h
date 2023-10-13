8dfe30b1bfc353840b3d564116187dc93cf079be
Hayden Williams <haydencwilliams26@gmail.com>
2023-10-13 09:47:48 -0400

    Initial Commit

---
+++ TestingPreferencePane/TestingPreferencePane.h
@@ -0,0 +1,14 @@
+//
+//  TestingPreferencePane.h
+//  TestingPreferencePane
+//
+//  Created by HAYDEN WILLIAMS on 10/13/23.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface TestingPreferencePane : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ TestingPreferencePane/TestingPreferencePane.m
@@ -0,0 +1,16 @@
+//
+//  TestingPreferencePane.m
+//  TestingPreferencePane
+//
+//  Created by HAYDEN WILLIAMS on 10/13/23.
+//
+
+#import "TestingPreferencePane.h"
+
+@implementation TestingPreferencePane
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
